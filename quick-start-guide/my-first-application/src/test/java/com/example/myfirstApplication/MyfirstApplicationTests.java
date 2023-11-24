1711b4ae1308adc34dc5bbf9e33148a0fd9adfe2
Harjaspreet-Singh <131734265+Harjaspreet-Singh@users.noreply.github.com>
2023-11-23 21:48:42 -0500

    Add files via upload

---
+++ quick-start-guide/my-first-application/src/test/java/com/example/myfirstApplication/MyfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
